eb71c97549c50848a5792d1dda72fd9e64d9f1d3
kywmsn <153073859+kywmsn@users.noreply.github.com>
2024-12-26 19:46:46 +0800

    Update script.js

--- script.js
+++ script.js
@@ -122,7 +122,7 @@ loginButton.addEventListener('click', () => {
             console.log('登录成功，用户信息：', userCredential.user);
             alert('登录成功！即将跳转...');
             setTimeout(() => {
-                window.location.replace("https://jiruan.xyz");
+                window.location.replace("https://jiruan.xyz/abc");
             }, 1000);
         })
         .catch((error) => {
